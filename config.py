0750404c0ffb6a82c9644162db00dcddb3f0da4b
wei <317694683@qq.com>
2018-02-22 01:09:18 +0800

    add sql poll size

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 HOST = '0.0.0.0'
 PORT = 9577
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:数据库密码@localhost/quiz'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:***@localhost/quiz'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_POOL_SIZE = 100
+SQLALCHEMY_POOL_RECYCLE = 3600
+
+DEBUG = False
 
 SECRET_KEY = 'b++d\x04~\xce\xf3\xb7#\xf3\xe9$H\xd9\x1c'
